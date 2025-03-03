491904191a8ccb450cb6f6c696af75c750ee8f2f
LukaLavs <168111567+LukaLavs@users.noreply.github.com>
2025-03-03 19:54:37 +0100

    Create Potence_relacij.py

---
+++ Potence_relacij.py
@@ -0,0 +1,17 @@
+def Relation(R, n):
+    """ izračuna R^n, kjer je R relacija oblike {(a,b), ...}
+    primer R = {(1, 2), (2, 3)} ali R = {("a", "b"), ("b", "c")} """
+    # A je tak, da R ⊆ A × A, | je operator unije
+    A = set(i for (i, j) in R) | set(j for (i, j) in R)
+    # Im_R je slika relacije R
+    Im_R = set(y for (x, y) in R)
+    K = R # Inicializacija pred začetkom zanke
+    for i in range(n-1): # i = 0,...,(n-2)
+        K = {(x, z) for x in A for z in A if any((x, y) in R and (y, z) in K for y in Im_R)}
+        # zdaj je K = R^(i+2); po definiciji kompozituma K <- K ° R
+    return K
+   
+# nastavi n in R in zaženi kodo    
+R = {(1,2),(2,3),(2,5),(3,1),(3,4),(4,2),(5,1),(5,4)}  
+n = 2
+#print(f"R^{n} = {Relation(R, n)}\n")
