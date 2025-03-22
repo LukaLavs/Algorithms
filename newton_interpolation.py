235b68a9cba6584acce35433a66360680e62003e
LukaLavs <168111567+LukaLavs@users.noreply.github.com>
2025-03-22 11:50:32 +0100

    Update newton_interpolation.py

--- newton_interpolation.py
+++ newton_interpolation.py
@@ -1,7 +1,16 @@
-from math import factorial
+from math import factorial, sin
+from matplotlib import pyplot as plt
+from fractions import Fraction
 
-def last_index(x, lst):
-    return len(lst) - lst[::-1].index(x) - 1
+def use_fractions(numbers):
+    """ Converts list of numbers to Fraction class """
+    return [Fraction(num) if not isinstance(num, Fraction) else num for num in numbers]
+
+def sort_values(x_values, y_values):
+    """ Sorts the points and prepares them for use """
+    T = sorted(enumerate(x_values), key=lambda x: x[1])
+    x_values, y_values = [x for (i, x) in T], [y_values[i] for (i, x) in T]
+    return x_values, y_values
 
 def k_th_derivate(x, k, x_values, y_values):
     """ Finds k-th derivate in y_values """
@@ -18,14 +27,14 @@ def deljena_diferenca(D, x_values, y_values):
     else:
         return ( deljena_diferenca(D[1:], x_values, y_values) - deljena_diferenca(D[:(k-1)], x_values, y_values) ) / (D[-1] - D[0])
     
-def F(x_values):
+def F(x_values, f):
     """ vectorizes f function """
-    def f(x):
-        return 4 / (1 + x)
     return [f(x) for x in x_values]
     
-def coefficients(x_values, y_values):
+def coefficients(x_values, y_values, fractions=False):
     """ Returns coefficients of Newton interpolation polinom """
+    x_values, y_values = sort_values(x_values=x_values, y_values=y_values)
+    if fractions: x_values, y_values = use_fractions(x_values), use_fractions(y_values)
     return [deljena_diferenca(D=x_values[0:i], x_values=x_values, y_values=y_values)
             for i in range(1, len(x_values) + 1)]  
      
@@ -36,9 +45,59 @@ def modified_horner(x, coefficients, x_values):
         v = coefficients[-i] + (x - x_values[-i]) * v
     return v
     
+def Linspace(a, b, n):
+    return [a + (b-a)*i/(n-1) for i in range(n)]  
+    
+def plot(x_values, y_values, A=1, B=1, f=None, y_min=None, y_max=None, fractions=False, smoothness=100):
+    """
+    Plots a given function with specified bleed amounts and axis limits.
+
+    Parameters:
+    A (float): The amount to bleed on the left side of the plot.
+    B (float): The amount to bleed on the right side of the plot.
+    f (function): The function to be plotted.
+    y_min (float): The minimum value for the y-axis.
+    y_max (float): The maximum value for the y-axis.
+    fractions (bool, optional): If True, performs exact calculations using fractions. Default is False.
+    smoothness (int, optional): The number of points to generate for a smoother curve. Default is 100.
+
+    Returns:
+    None
+    """
+    indexes = []
+    x_prev = None
+    for i in range(len(x_values)):
+        x_current = x_values[i]
+        if x_current == x_prev:
+            indexes.append(i)
+        x_prev = x_current
+    X_points = [x_values[i] for i in range(len(x_values)) if not i in indexes]
+    Y_points = [y_values[i] for i in range(len(y_values)) if not i in indexes]
+    coef = coefficients(x_values=x_values, y_values=y_values, fractions=fractions)
+    print("coefficients: ", [str(c) for c in coef])
+    a, b = min(X_points), max(X_points)
+    X = Linspace(a - A, b + B, smoothness*int(b - a + A + B))
+    Y = [modified_horner(x, coef, x_values) for x in X]
+    plt.figure(figsize=(8, 5))
+    plt.plot(X, Y, label='p(x)', color='blue')
+    if f: plt.plot(X, F(X, f), label='f(x)', color='black')
+    plt.scatter(X_points, Y_points, color='red', label='Interpolation points')
+    if y_min and y_max: plt.ylim(y_min, y_max)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Newton interpolation')
+    plt.legend()
+    plt.show()
+    
+    
+    
+
+            
     
 ##################### TEST ##############################
-K = coefficients(x_values=[0, 0, 0, 1, 1, 1], y_values=[4, -4, 8, 2, -1, 1])
-K = coefficients(x_values=[1, 2, 3, 4], y_values=[1, 2, 5, 3, 8])
-x_values=[1, 2, 3, 4]
-print(modified_horner(0, K, x_values))
+# Razvijmo funkcijo sin(x) v Taylorjevo vrsto z uporabo Newtonove interpolacije
+def f(x): return sin(x)
+n = 25
+x_values = [0 for _ in range(n)]
+y_values = [(-i%2)*(-1 if i%4==3 else 1) for i in range(n)]
+plot(x_values=x_values, y_values=y_values, A=10, B=10, f=f, y_min=-1.1, y_max=1.1, fractions=True)
